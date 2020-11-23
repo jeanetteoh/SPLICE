e36368b348f6a763d6aee0307fdb243684d6b480
Hongan Zhang <hzhan265@ucr.edu>
2020-11-22 23:41:38 -0800

    update class and separate different file into scr file and header file

---
+++ header/restaurantSort.hpp
@@ -0,0 +1,13 @@
+#ifndef _RESTAURANT_SORT_
+#define  _RESTAURANT_SORT_
+#include "../header/restaurantSort.hpp"
+#include "../header/sortbyName.hpp"
+#include "../header/sortbyPrice.hpp"
+#include "../header/sortbyType.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#endif // 
